8ad47381e4c049d5e9295347629ffc4f29ddf669
CCharles99 <ccha0057@student.monash.edu>
2024-05-13 16:15:06 +1000

    removed node.js backend

--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const Router = express.Router();
-
-Router
-    .use('/test-route', require('./test-route'));
-
-module.exports = Router;
--- backend/routes/test-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req,res) => {
-    console.log('request made')
-    res.json('hello world')
-})
-
-module.exports = router;
