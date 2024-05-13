4889b5fc305ec89999239358c203cf45b2aa5205
CCharles99 <ccha0057@student.monash.edu>
2024-05-13 15:59:39 +1000

    added map

--- dav-app/src/App.js
+++ dav-app/src/App.js
@@ -1,27 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 import TestPage from './components/TestPage';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2NoYTAwNTciLCJhIjoiY2swOWZ5ODVwMDhjYTNjbnljN3Z5MXI4ayJ9._ibGMEIebSWPwmIEbUHc6A';
+
+
 function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(144.9);
+  const [lat, setLat] = useState(-37.8);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      center: [lng, lat],
+      zoom: zoom,
+      projection: 'mercator',
+      maxBounds: [[-180, -90], [180, 90]]
+    });
+  });
+  
   return (
     <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div ref={mapContainer} className="map-container" />
       </div>
       <Routes>
         <Route path="/testpage" element = {<TestPage/>}/>
